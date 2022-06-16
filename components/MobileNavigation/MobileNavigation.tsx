560d8b0503ce5b04a3e591c166b39878d4a5e6a8
Timo Schmidt <schmidt@timo.email>
2022-06-16 16:21:18 +0200

    Icon and Styling Fixes

--- components/MobileNavigation/MobileNavigation.tsx
+++ components/MobileNavigation/MobileNavigation.tsx
@@ -1,5 +1,12 @@
 import React from "react";
-import { MdMeetingRoom, MdListAlt, MdPerson } from "react-icons/md";
+import {
+  MdOutlineMeetingRoom,
+  MdMeetingRoom,
+  MdInsertChartOutlined,
+  MdInsertChart,
+  MdPersonOutline,
+  MdPerson,
+} from "react-icons/md";
 
 type NavItemProps = {
   id: string;
@@ -12,7 +19,7 @@ const NavItem = ({ id, active, children, onSelect }: NavItemProps) => {
   return (
     <button
       className={`w-full p-3 flex flex-col items-center justify-center space-y-1 max-w-lg text-xs ${
-        active ? "bg-gray-300 text-black" : "text-gray-500"
+        active ? "font-medium text-black" : "text-gray-500"
       }`}
       onClick={() => onSelect(id)}
     >
@@ -20,6 +27,8 @@ const NavItem = ({ id, active, children, onSelect }: NavItemProps) => {
     </button>
   );
 };
+
+const navIds = ["NAV_MEETINGS", "NAV_REPORTS", "NAV_PROFILE"];
 
 type Props = {
   activeItemId: string;
@@ -35,26 +44,40 @@ const MobileNavigation = ({ activeItemId, onSelect }: Props) => {
       onClick={() => onSelect(activeItem)}
     >
       <NavItem
-        active={activeItem === "nav_meetings"}
-        id="nav_meetings"
+        active={activeItem === navIds[0]}
+        id={navIds[0]}
         onSelect={(id) => setActiveItem(id)}
       >
-        <MdMeetingRoom className="text-gray-500 w-5 h-5" />
+        {activeItem === navIds[0] ? (
+          <MdMeetingRoom className="w-5 h-5 text-black" />
+        ) : (
+          <MdOutlineMeetingRoom className="w-5 h-5 text-gray-500" />
+        )}
         <p>Meetings</p>
       </NavItem>
       <NavItem
-        active={activeItem === "nav_reports"}
-        id="nav_reports"
+        active={activeItem === navIds[1]}
+        id={navIds[1]}
         onSelect={(id) => setActiveItem(id)}
       >
-        <MdListAlt className="text-gray-500 w-5 h-5" /> <p>Meeting Reports</p>
+        {activeItem === navIds[1] ? (
+          <MdInsertChart className="w-5 h-5 text-black" />
+        ) : (
+          <MdInsertChartOutlined className="w-5 h-5 text-gray-500" />
+        )}
+        <p>Meeting Reports</p>
       </NavItem>
       <NavItem
-        active={activeItem === "nav_profile"}
-        id="nav_profile"
+        active={activeItem === navIds[2]}
+        id={navIds[2]}
         onSelect={(id) => setActiveItem(id)}
       >
-        <MdPerson className="text-gray-500 w-5 h-5" /> <p>Profil</p>
+        {activeItem === navIds[2] ? (
+          <MdPerson className="w-5 h-5 text-black" />
+        ) : (
+          <MdPersonOutline className="w-5 h-5 text-gray-500" />
+        )}
+        <p>Profil</p>
       </NavItem>
     </div>
   );

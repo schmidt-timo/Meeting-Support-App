c66cde2310f63661282219d27384f1f31bae7000
Timo Schmidt <schmidt@timo.email>
2022-06-19 01:30:22 +0200

    Add QR code scanner

--- pages/addMeeting.tsx
+++ pages/addMeeting.tsx
@@ -4,21 +4,50 @@ import Input from "../components/formElements/Input";
 import Label from "../components/formElements/Label";
 import LabelInputWrapper from "../components/formElements/LabelInputWrapper";
 import SubviewBuilder from "../components/SubviewBuilder/SubviewBuilder";
+import { QrReader } from "react-qr-reader";
+import { useState } from "react";
 
 const AddMeeting: NextPage = () => {
+  const [qrcode, setQrcode] = useState("No result");
+
   return (
     <SubviewBuilder title="Add meeting" backButtonPath="/">
       <div className="space-y-10">
         <LabelInputWrapper>
           <Label>Add a meeting by scanning the qr code</Label>
-          <div className="bg-black rounded-xl h-60" />{" "}
+          <div className="rounded-xl">
+            <QrReader
+              className="w-full"
+              onResult={(result, error) => {
+                if (!!result) {
+                  setQrcode(result?.getText());
+                }
+                if (!!error) {
+                  console.info(error);
+                }
+              }}
+              videoContainerStyle={{
+                backgroundColor: "black",
+                borderRadius: "0.75rem",
+                width: "100%",
+              }}
+              constraints={{
+                facingMode: "environment",
+                aspectRatio: {
+                  ideal: 1.777,
+                },
+              }}
+            />
+          </div>
         </LabelInputWrapper>
         <LabelInputWrapper>
           <Label>or manually via Meeting ID</Label>
-          <Input placeholder="Meeting ID" />
-          <Button onClick={() => {}}>Add meeting</Button>
+          <Input placeholder="Meeting ID" value={qrcode} />
         </LabelInputWrapper>
       </div>
+      <Button highlighted onClick={() => {}}>
+        Add meeting
+      </Button>
     </SubviewBuilder>
   );
 };

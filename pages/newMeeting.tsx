dd9227d5a1fdc83e9af1367ae1199293eac397c3
Timo Schmidt <schmidt@timo.email>
2022-06-23 02:55:25 +0200

    Add component for meeting creation

--- pages/newMeeting.tsx
+++ pages/newMeeting.tsx
@@ -1,21 +1,35 @@
 import { NextPage } from "next";
 import { useRouter } from "next/router";
 import { useState } from "react";
-import CreateMeetingPage from "../components/views/CreateMeetingPage";
+import NewMeetingPage from "../components/views/NewMeetingPage";
+import ManageAgenda from "../components/views/ManageAgenda";
+import { MeetingAgendaItem } from "../utils/types";
 
 type Views = "CREATE_MEETING" | "MANAGE_AGENDA" | "MANAGE_PARTICIPANTS";
 
 const NewMeeting: NextPage = () => {
   const router = useRouter();
   const [currentView, setCurrentView] = useState<Views>("CREATE_MEETING");
+  const [agendaItems, setAgendaItems] = useState<MeetingAgendaItem[]>([]);
 
   return (
     <>
       {currentView === "CREATE_MEETING" && (
-        <CreateMeetingPage
-          onManageAgenda={() => setCurrentView("MANAGE_AGENDA")}
-          onManageParticipants={() => setCurrentView("MANAGE_PARTICIPANTS")}
-          onCreateMeeting={(meeting) => {}}
+        <NewMeetingPage
+          onNext={(meeting) => {
+            // TODO: Save meeting
+            setCurrentView("MANAGE_AGENDA");
+          }}
+          onClose={() => router.push("/")}
+        />
+      )}
+      {currentView === "MANAGE_AGENDA" && (
+        <ManageAgenda
+          agendaItems={agendaItems}
+          onBack={(items) => {
+            setAgendaItems(items);
+            setCurrentView("CREATE_MEETING");
+          }}
           onClose={() => router.push("/")}
         />
       )}

7d0d088f50986d42bcd3ec77079f9634bbba8c0c
Timo Schmidt <schmidt@timo.email>
2022-06-23 02:56:08 +0200

    Update SubviewBuilder component

--- components/SubviewBuilder/SubviewBuilder.tsx
+++ components/SubviewBuilder/SubviewBuilder.tsx
@@ -4,40 +4,29 @@ import { MdOutlineClose, MdKeyboardBackspace } from "react-icons/md";
 type Props = {
   title: string;
   children: React.ReactNode;
-  variant?: "CLOSE" | "ARROW";
   onClose: () => void;
+  onBack?: () => void;
 };
 
-const SubviewBuilder = ({
-  title,
-  children,
-  variant = "CLOSE",
-  onClose,
-}: Props) => {
+const SubviewBuilder = ({ title, children, onClose, onBack }: Props) => {
   return (
     <div className="w-full min-h-screen flex flex-col bg-gray-200 px-6 pb-6">
-      <div
-        className={`flex items-center py-5 ${
-          variant === "CLOSE" && "p-1 justify-between"
-        } ${variant === "ARROW" && "justify-start space-x-3"}`}
-      >
-        {variant === "ARROW" && (
+      <div className="flex py-5 justify-between items-center space-x-3">
+        {onBack && (
           <button
-            onClick={onClose}
-            className="rounded-full p-1 bg-white hover:bg-black group"
+            onClick={onBack}
+            className="rounded-full p-1 bg-black hover:bg-black group"
           >
-            <MdKeyboardBackspace className="text-black w-6 h-6 group-hover:text-white" />
+            <MdKeyboardBackspace className="text-white w-6 h-6 group-hover:text-white" />
           </button>
         )}
-        <h1 className="font-bold text-2xl">{title}</h1>
-        {variant === "CLOSE" && (
-          <button
-            onClick={onClose}
-            className="rounded-full p-1 bg-white hover:bg-black group"
-          >
-            <MdOutlineClose className="text-black w-6 h-6 group-hover:text-white" />
-          </button>
-        )}
+        <h1 className="font-bold text-xl">{title}</h1>
+        <button
+          onClick={onClose}
+          className="rounded-full p-1 bg-black hover:bg-black group"
+        >
+          <MdOutlineClose className="text-white w-6 h-6 group-hover:text-white" />
+        </button>
       </div>
       <div className="w-full flex flex-col flex-1 justify-between">
         {children}

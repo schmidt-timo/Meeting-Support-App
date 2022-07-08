ed7556e204471eb874dc9f9a3c52f287e0e7f3f2
Timo Schmidt <schmidt@timo.email>
2022-07-08 21:05:55 +0200

    Update loading animation

--- components/LoadingScreen/LoadingScreen.tsx
+++ components/LoadingScreen/LoadingScreen.tsx
@@ -1,9 +1,14 @@
+import { MdAccessTime } from "react-icons/md";
+
 type Props = {};
 
 const LoadingScreen = ({}: Props) => {
   return (
     <div className="w-full h-screen flex items-center justify-center">
-      Loading ...
+      <div className="flex flex-col space-y-2 items-center justify-center py-5 px-10 bg-gray-100 rounded-xl">
+        <MdAccessTime className="animate-spin w-10 h-10 text-gray-400" />
+        <p className="text-xs uppercase text-gray-400">Loading</p>
+      </div>
     </div>
   );
 };

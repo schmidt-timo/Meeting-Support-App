81ff97bb852046706484da47bac501246e36593b
Timo Schmidt <schmidt@timo.email>
2022-07-19 21:53:51 +0200

    Modal Fix

--- components/pages/meetings/MeetingOverviewPage.tsx
+++ components/pages/meetings/MeetingOverviewPage.tsx
@@ -44,24 +44,7 @@ const MeetingOverviewPage = ({
   const [showModal, setShowModal] = useState(false);
 
   return (
-    <PageLayout
-      header={{
-        title: "Meetings",
-        buttons: [
-          {
-            id: "HEADER_BTN_QR",
-            icon: <MdQrCodeScanner className="w-6 h-6" />,
-            onClick: onAddMeeting,
-          },
-          {
-            id: "HEADER_BTN_NEWMEETING",
-            icon: <MdOutlineAdd className="w-8 h-8" />,
-            onClick: onCreateMeeting,
-          },
-        ],
-      }}
-      activeNavItemId={NAVIGATION_IDS.meetings}
-    >
+    <>
       {showModal && (
         <Modal
           title="Meeting has not started yet"
@@ -79,57 +62,90 @@ const MeetingOverviewPage = ({
         </Modal>
       )}
 
-      <div className="px-3 space-y-3">
-        {!meetings ||
-          (ownMeetings.length < 1 && otherMeetings.length < 1 && (
-            <InfoTextBox title="No meetings found">
-              Add new meetings by using the QR code scanner with the button on
-              the top or add them manually by entering the meeting ID. You can
-              also create your own meetings by using the plus button.
-            </InfoTextBox>
-          ))}
-        {!!ownMeetings.length && (
-          <Accordion
-            title={`${MEETING_CATEGORY_LABELS.yourMeetings} (${ownMeetings.length})`}
-          >
-            {ownMeetings.map((m) => (
-              <MeetingInfoBox
-                key={m.id}
-                userId={userId}
-                meeting={m}
-                onEdit={() => router.push(`${m.id}/edit`)}
-                onManageAgenda={() => router.push(`${m.id}/agenda`)}
-                onManageParticipants={() => router.push(`${m.id}/participants`)}
-                onStartMeeting={() => router.push(`${m.id}/start`)}
-              />
+      <PageLayout
+        header={{
+          title: "Meetings",
+          buttons: [
+            {
+              id: "HEADER_BTN_QR",
+              icon: <MdQrCodeScanner className="w-6 h-6" />,
+              onClick: onAddMeeting,
+            },
+            {
+              id: "HEADER_BTN_NEWMEETING",
+              icon: <MdOutlineAdd className="w-8 h-8" />,
+              onClick: onCreateMeeting,
+            },
+          ],
+        }}
+        activeNavItemId={NAVIGATION_IDS.meetings}
+      >
+        <div className="px-3 space-y-3">
+          {!meetings ||
+            (ownMeetings.length < 1 && otherMeetings.length < 1 && (
+              <InfoTextBox title="No meetings found">
+                Add new meetings by using the QR code scanner with the button on
+                the top or add them manually by entering the meeting ID. You can
+                also create your own meetings by using the plus button.
+              </InfoTextBox>
             ))}
-          </Accordion>
-        )}
-        {!!otherMeetings.length && (
-          <Accordion
-            title={`${MEETING_CATEGORY_LABELS.otherMeetings} (${otherMeetings.length})`}
-          >
-            {otherMeetings.map((m) => (
-              <MeetingInfoBox
-                key={m.id}
-                userId={userId}
-                meeting={m}
-                onManageAgenda={() => router.push(`${m.id}/agenda`)}
-                onManageParticipants={() => router.push(`${m.id}/participants`)}
-                onViewDetails={() => router.push(`${m.id}/details`)}
-                onStartMeeting={() => {
-                  if (is10MinutesBeforeMeetingOrLater(new Date(m.startDate))) {
-                    router.push(`${m.id}/start`);
-                  } else {
-                    setShowModal(true);
+          {!!ownMeetings.length && (
+            <Accordion
+              title={`${MEETING_CATEGORY_LABELS.yourMeetings} (${ownMeetings.length})`}
+            >
+              {ownMeetings.map((m) => (
+                <MeetingInfoBox
+                  key={m.id}
+                  userId={userId}
+                  meeting={m}
+                  onEdit={() => router.push(`${m.id}/edit`)}
+                  onManageAgenda={() => router.push(`${m.id}/agenda`)}
+                  onManageParticipants={() =>
+                    router.push(`${m.id}/participants`)
                   }
-                }}
-              />
-            ))}
-          </Accordion>
-        )}
-      </div>
-    </PageLayout>
+                  onStartMeeting={() => {
+                    if (
+                      is10MinutesBeforeMeetingOrLater(new Date(m.startDate))
+                    ) {
+                      router.push(`${m.id}/start`);
+                    } else {
+                      setShowModal(true);
+                    }
+                  }}
+                />
+              ))}
+            </Accordion>
+          )}
+          {!!otherMeetings.length && (
+            <Accordion
+              title={`${MEETING_CATEGORY_LABELS.otherMeetings} (${otherMeetings.length})`}
+            >
+              {otherMeetings.map((m) => (
+                <MeetingInfoBox
+                  key={m.id}
+                  userId={userId}
+                  meeting={m}
+                  onManageAgenda={() => router.push(`${m.id}/agenda`)}
+                  onManageParticipants={() =>
+                    router.push(`${m.id}/participants`)
+                  }
+                  onViewDetails={() => router.push(`${m.id}/details`)}
+                  onStartMeeting={() => {
+                    if (
+                      is10MinutesBeforeMeetingOrLater(new Date(m.startDate))
+                    ) {
+                      router.push(`${m.id}/start`);
+                    } else {
+                      setShowModal(true);
+                    }
+                  }}
+                />
+              ))}
+            </Accordion>
+          )}
+        </div>
+      </PageLayout>
+    </>
   );
 };
 

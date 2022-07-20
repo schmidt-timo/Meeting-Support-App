b8d66f6800ecb373265be9e4c392d315e2afd186
Timo Schmidt <schmidt@timo.email>
2022-07-21 00:17:19 +0200

    Env Fix

--- lib/supabase/config.ts
+++ lib/supabase/config.ts
@@ -8,5 +8,5 @@ export const supabase = createClient(
 export const getServiceSupabase = () =>
   createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL || "",
-    process.env.SUPABASE_SERVICE_ROLE_KEY || ""
+    process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY || ""
   );

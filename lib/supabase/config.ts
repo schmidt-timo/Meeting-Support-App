3564e6b8ba92a1251d4e9707069a298bdd9d0658
Timo Schmidt <schmidt@timo.email>
2022-07-21 00:30:46 +0200

    Vercel Fix

--- lib/supabase/config.ts
+++ lib/supabase/config.ts
@@ -1,12 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL || "",
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ""
-);
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
+const supabaseServiceRoleKey = process.env
+  .NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY as string;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export const getServiceSupabase = () =>
-  createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL || "",
-    process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY || ""
-  );
+  createClient(supabaseUrl, supabaseServiceRoleKey);
